9dfcdbd40b61cb2fe4367b4259d169ef9ee66365
William Soukkachang <WSoukkachang@yahoo.com>
2019-07-09 21:51:22 -0500

    intial commit.

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('the enhancer', () => {
+
+    describe('the repair function', () => {
+        it('should restore durability', () => {
+            //arrange
+            const longsword ={health: 100};
+
+            //act
+            const result = enhancer.repair(longsword);
+
+            //assert
+            expect(result).toBe(100);
+        });
+    })
+
+
+});
